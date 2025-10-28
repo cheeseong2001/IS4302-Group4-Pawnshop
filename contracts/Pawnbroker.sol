8833e122a0ee6036350ae5ad7c2494d93b8f02aa
luan-chee-seong <e0957617@u.nus.edu>
2025-10-28 12:47:00 +0800

    Add pawnbroker.sol

---
+++ contracts/Pawnbroker.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./PawnshopCommon.sol";
+
+contract Pawnbroker is PawnshopCommon {
+    event ItemClaimed(uint256 indexed itemId, address indexed taker);
+    event ClaimAccepted(uint256 indexed itemId, address indexed owner);
+    event ItemDelivered(uint256 indexed itemId, address indexed taker);
+
+    constructor(address _pawnStorageAddress) PawnshopCommon(_pawnStorageAddress) {}
+
+    // ---- Modifiers ----
+    modifier itemTakerOnly(uint256 itemId) {
+        require(pawnStorageContract.getItemTaker(itemId) == msg.sender, "Sender must be the item taker");
+        _;
+    }
+
+    modifier notItemOwner(uint256 itemId) {
+        require(pawnStorageContract.getItemOwner(itemId) != msg.sender, "Taker must not be the item owner");
+        _;
+    }
+
+    function getMyClaimedList() external view returns (PawnStorage.PawnItem[] memory) {
+        return pawnStorageContract.getItemsByTaker(msg.sender);
+    }
+
+    function startClaimProcess(uint256 itemId) internal {
+        // Helper function to update details upon claim request
+        pawnStorageContract.setStatus(itemId, PawnStorage.ItemStatus.IN_NEGOTIATION);
+
+        pawnStorageContract.setOtherParty(itemId, msg.sender);
+    }
+
+    function claimItem(
+        uint256 itemId
+    ) external payable notItemOwner(itemId) itemStatusIs(itemId, PawnStorage.ItemStatus.LISTED) {
+        (uint256 price, , uint256 punishment) = pawnStorageContract.getItemPrices(itemId);
+
+        require(msg.value >= (price + punishment), "Insufficient ether to claim");
+
+        uint256 toReturn = msg.value - (price + punishment);
+        if (toReturn > 0) {
+            (bool success, ) = payable(msg.sender).call{value: toReturn}("");
+            require(success, "Transfer failed");
+        }
+
+        startClaimProcess(itemId);
+    }
+
+    function confirmItemDelivered(uint256 itemId) external itemStatusIs(itemId, PawnStorage.ItemStatus.IN_DELIVERY) {
+        (uint256 price, , ) = pawnStorageContract.getItemPrices(itemId);
+        address itemOwner = pawnStorageContract.getItemOwner(itemId);
+
+        (bool success, ) = payable(itemOwner).call{value: price}("");
+        require(success, "Transfer failed");
+
+        pawnStorageContract.setStatus(itemId, PawnStorage.ItemStatus.CLAIMED);
+        pawnStorageContract.setTakenBy(itemId, msg.sender);
+        pawnStorageContract.setTakenAt(itemId, block.timestamp);
+    }
+
+    function returnItem(
+        uint256 itemId
+    ) external itemTakerOnly(itemId) itemStatusIs(itemId, PawnStorage.ItemStatus.IN_REDEMPTION) {
+        pawnStorageContract.setStatus(itemId, PawnStorage.ItemStatus.IN_DELIVERY_RETURN);
+    }
+}
