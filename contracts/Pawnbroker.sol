66c75e35155eb7eb439bbda64980b3162c3c608c
luan-chee-seong <e0957617@u.nus.edu>
2025-10-28 18:10:18 +0800

    Add pawnbroker withdraw feature

--- contracts/Pawnbroker.sol
+++ contracts/Pawnbroker.sol
@@ -52,6 +52,15 @@ contract Pawnbroker is PawnshopCommon {
         startClaimProcess(itemId);
     }
 
+    function withdrawClaim(
+        uint256 itemId
+    ) public claimInitiatorOnly(itemId) itemStatusIs(itemId, PawnStorage.ItemStatus.IN_NEGOTIATION) {
+        (uint256 price, , uint256 punishment) = pawnStorageContract.getItemPrices(itemId);
+        (bool success, ) = payable(msg.sender).call{value: price + punishment}("");
+        require(success, "Transfer failed");
+        pawnStorageContract.setStatus(itemId, PawnStorage.ItemStatus.LISTED);
+    }
+
     function confirmItemDelivered(
         uint256 itemId
     ) external claimInitiatorOnly(itemId) itemStatusIs(itemId, PawnStorage.ItemStatus.IN_DELIVERY) {
@@ -74,23 +83,13 @@ contract Pawnbroker is PawnshopCommon {
         pawnStorageContract.removeItemIdFromTakerList(itemId);
     }
 
-    function claimAmount(uint256 itemId) external itemTakerOnly(itemId) {
-        PawnStorage.ItemStatus itemStatus = pawnStorageContract.getItemStatus(itemId);
-        require(
-            itemStatus == PawnStorage.ItemStatus.IN_NEGOTIATION || itemStatus == PawnStorage.ItemStatus.RETURNED,
-            "Claim not allowed for this item status"
-        );
-
-        (uint256 price, uint256 redemption, uint256 punishment) = pawnStorageContract.getItemPrices(itemId);
-        if (itemStatus == PawnStorage.ItemStatus.IN_NEGOTIATION) {
-            (bool success, ) = payable(msg.sender).call{value: price + punishment}("");
-            require(success, "Transfer failed");
-            pawnStorageContract.setStatus(itemId, PawnStorage.ItemStatus.LISTED);
-        } else if (itemStatus == PawnStorage.ItemStatus.RETURNED) {
-            (bool success, ) = payable(msg.sender).call{value: redemption + punishment}("");
-            require(success, "Transfer failed");
-            pawnStorageContract.setStatus(itemId, PawnStorage.ItemStatus.END_OF_TRANSACTION);
-        }
+    function claimAmount(
+        uint256 itemId
+    ) external itemTakerOnly(itemId) itemStatusIs(itemId, PawnStorage.ItemStatus.RETURNED) {
+        (, uint256 redemption, uint256 punishment) = pawnStorageContract.getItemPrices(itemId);
+        (bool success, ) = payable(msg.sender).call{value: redemption + punishment}("");
+        require(success, "Transfer failed");
+        pawnStorageContract.setStatus(itemId, PawnStorage.ItemStatus.END_OF_TRANSACTION);
     }
 
     receive() external payable {
